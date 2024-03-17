8483300584cad8c42bac0c408e22dbc3ded6efd6
Charlatan92 <xcharlatan92@gmail.com>
2024-03-17 17:25:54 +0100

    Ajout de route

--- src/Controller/GalerieController.php
+++ src/Controller/GalerieController.php
@@ -1,24 +1,23 @@
 <?php
-//src/Controller/GalerieController.php
+// src/Controller/GalerieController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class GalerieController extends AbstractController
 {
-    #[Route('/Galerie', name: 'galerie')]
-    public function index(): Response
+    public function index($page = 1): Response
     {
-        $images = [
-            'image1.jpg',
-            'image2.jpg',
-            'image3.jpg',
-            'image4.jpg',
-            'image5.jpg',
-            'image6.jpg'
-        ];
+        $imagesPerPage = 6;
+        $startIndex = ($page - 1) * $imagesPerPage;
+
+        $allImages = [];
+        for ($i = 1; $i <= 18; $i++) {
+            $allImages[] = "image$i.jpg";
+        }
+
+        $images = array_slice($allImages, $startIndex, $imagesPerPage);
 
         return $this->render('galerie/index.html.twig', [
             'images' => $images,
